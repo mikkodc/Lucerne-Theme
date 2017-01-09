ea81184f231d1d6a0e9c87540bd74861de5cb772
MikkoDC <mikkodecastro@ymail.com>
2017-01-09 14:13:23 +0800

    Fix Text Styling on Frontpage

--- template-parts/content-ajax.php
+++ template-parts/content-ajax.php
@@ -8,5 +8,12 @@
     </div>
   </a>
   <h3 class="article-title"><a class="ajax-link" data-id="<?php echo get_the_ID(); ?>"><?php the_title(); ?></a></h3>
-  <p class="article-excerpt"><?php echo wp_trim_words( get_the_content(), 40, ''); ?></p>
+  <p class="article-excerpt">
+      <?php $article_type = get_field("article_type", $queried_post); ?>
+      <?php //echo $article_type == 1 ? '<p>' .wp_trim_words( get_field("article_summary", $queried_post), 40, ''). '</p>' : wp_trim_words( get_field("article_content", $queried_post), 40, ''); ?>
+      <?php echo wp_trim_words( get_field("article_content", $queried_post), 40, ''); ?>
+      <?php echo wp_trim_words( get_field("article_summary", $queried_post), 40, '') ?>
+
+
+  </p>
 </div>
